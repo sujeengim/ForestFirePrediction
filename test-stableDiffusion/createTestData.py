ac2566d4db58423f4f14ee0e9d4e7b35ffe329c9
sujeengim <ksj030120@gmail.com>
2024-11-15 00:51:59 +0900

    '수정'

--- test-stableDiffusion/createTestData.py
+++ test-stableDiffusion/createTestData.py
@@ -1,11 +1,14 @@
+'''
+프롬프트 테스트할 이미지를 생성하는 파일
+'''
 import os
 from PIL import Image
 
-in1 = 'C:\\Users\\user\\Desktop\\ksj\\ForestFirePrediction\\test-stableDiffusion\\jm-data\\train'
-in2 = 'C:\\Users\\user\\Desktop\\ksj\\ForestFirePrediction\\test-stableDiffusion\\jm-data\\val'
+in1 = 'test-stableDiffusion/jm-data/train'
+in2 = 'test-stableDiffusion/jm-data/val'
 
-out1 = 'C:\\Users\\user\\Desktop\\ksj\\ForestFirePrediction\\test-stableDiffusion\\jm-test-data\\train'
-out2 = 'C:\\Users\\user\\Desktop\\ksj\\ForestFirePrediction\\test-stableDiffusion\\jm-test-data\\val'
+out1 = 'test-stableDiffusion/jm-test-data/train'
+out2 = 'test-stableDiffusion/jm-test-data/val'
 
 
 def save1(input, class_name):
@@ -13,22 +16,22 @@ def save1(input, class_name):
         if i>70:
             break
         print(i)
-        os.makedirs(f'{out1}\\{class_name}', exist_ok=True)
-        image = Image.open(f'{input}\\{img}')
+        os.makedirs(f'{out1}/{class_name}', exist_ok=True)
+        image = Image.open(f'{input}/{img}')
         # image.show()
         
-        image.save(f'{out1}\\{class_name}\\{img}', 'JPEG')
+        image.save(f'{out1}/{class_name}/{img}', 'JPEG')
 def save2(input, class_name):
     for i, img in enumerate(os.listdir(input)):
         if i>30:
             break
         print(i)
-        os.makedirs(f'{out2}\\{class_name}', exist_ok=True)
-        image = Image.open(f'{input}\\{img}')
+        os.makedirs(f'{out2}/{class_name}', exist_ok=True)
+        image = Image.open(f'{input}/{img}')
         # image.show()
         
-        image.save(f'{out2}\\{class_name}\\{img}', 'JPEG')
+        image.save(f'{out2}/{class_name}/{img}', 'JPEG')
 
 for class_name in ['fire', 'no_fire']:
-    save1(in1+'\\'+class_name, class_name)
-    save2(in2+'\\'+class_name, class_name)
+    save1(in1+'/'+class_name, class_name)
+    save2(in2+'/'+class_name, class_name)
